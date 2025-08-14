457916d56c26c043cdda2b35fc39de2cf260513d
Amy DiGiovanni <amydigiov@gmail.com>
2025-08-14 15:48:16 -0700

    implement playing of all active notes

--- Assets/Scripts/NoteUtils.cs
+++ Assets/Scripts/NoteUtils.cs
@@ -1,7 +1,6 @@
-using Melanchall.DryWetMidi.MusicTheory;
-
 public static class NoteUtils
 {
+    // source: https://github.com/IRCSS/Procedural-Sound/blob/master/Assets/Scripts/Maps/KeyIndeciesToFrequencies.cs
     public static float[] FundementalFrequencies = new float[]
     {   // C      // CSharp                                                                                           // B
         16.35f,   17.32f,   18.35f,   19.45f,   20.60f,   21.83f,   23.12f,   24.50f,   25.96f,   27.50f,   29.14f,   30.87f, // 0
@@ -14,9 +13,4 @@ public static class NoteUtils
         2093.00f, 2217.46f, 2349.32f, 2489.02f, 2637.02f, 2793.83f, 2959.96f, 3135.96f, 3322.44f, 3520.00f, 3729.31f, 3951.07f,
         4186.01f, 4434.92f, 4698.63f, 4978.03f, 5274.04f, 5587.65f, 5919.91f, 6271.93f, 6648.88f, 7040.00f, 7458.62f, 7902.14f // 8
     };
-
-    public static float GetFundamentalFrequency(this Note note)
-    {
-        return FundementalFrequencies[note.Octave * 12 + (int)note.NoteName];
-    }
 }
--- Assets/Scripts/Synth/MIDIInstrument.cs
+++ Assets/Scripts/Synth/MIDIInstrument.cs
@@ -1,39 +1,68 @@
+using Melanchall.DryWetMidi.Common;
 using Melanchall.DryWetMidi.MusicTheory;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
 public class MIDIInstrument : MonoBehaviour
 {
-    AudioSource m_AudioSource;
+    struct ActiveNoteInfo
+    {
+        public bool isActive;
+        public double onTime;
+        public double offTime;
+        public float fundamentalFrequency;
+    }
 
     int m_SampleRate;
 
+    ActiveNoteInfo[] m_ActiveNotes;
+
     void Awake()
     {
-        m_AudioSource = GetComponent<AudioSource>();
         m_SampleRate = AudioSettings.outputSampleRate;
+
+        m_ActiveNotes = new ActiveNoteInfo[SevenBitNumber.MaxValue];
+        var freqCount = NoteUtils.FundementalFrequencies.Length;
+        for (var i = 0; i < freqCount; i++)
+        {
+            m_ActiveNotes[i].fundamentalFrequency = NoteUtils.FundementalFrequencies[i];
+        }
+    }
+
+    public void NoteOn(Note note)
+    {
+        m_ActiveNotes[note.NoteNumber].isActive = true;
+        m_ActiveNotes[note.NoteNumber].onTime = AudioSettings.dspTime;
+    }
+
+    public void NoteOff(Note note)
+    {
+        m_ActiveNotes[note.NoteNumber].isActive = false;
+        m_ActiveNotes[note.NoteNumber].offTime = AudioSettings.dspTime;
     }
 
     void OnAudioFilterRead(float[] data, int channels)
     {
-        if (channels > 2) // don't know what to do with more than 2 channels
+        if (channels != 2) // currently only support for 2 channels
             return;
 
-        var frequency = Note.Get(NoteName.C, 4).GetFundamentalFrequency(); // C4
-
-        var firstPhase = (float)(AudioSettings.dspTime % (1.0d / (double)frequency));
-        var currentDataStep = 0;
-        for (var i = 0; i < data.Length; i++)
+        for (var i = 0; i < m_ActiveNotes.Length; i++)
         {
-            var phase = (firstPhase + (float)currentDataStep / m_SampleRate) * frequency;
-            var sample = Sample(phase);
-            data[i] += sample;
-            currentDataStep++;
+            var activeNote = m_ActiveNotes[i];
+            if (!activeNote.isActive)
+                continue;
 
-            if (channels == 2)
+            var frequency = activeNote.fundamentalFrequency;
+            var noteTime = AudioSettings.dspTime - activeNote.onTime;
+            var currentPhaseStart = (float)(noteTime % (1.0d / (double)frequency));
+            var currentDataStep = 0;
+            for (var j = 0; j < data.Length; j = j + 2)
             {
-                data[i + 1] = data[i];
-                i++;
+                var phase = (currentPhaseStart + (float)currentDataStep / m_SampleRate) * frequency;
+                var sample = Sample(phase);
+                data[j] += sample;
+                data[j + 1] = sample;
+                currentDataStep++;
             }
         }
     }
