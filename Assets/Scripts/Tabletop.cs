e4fca8617afd5d1c4218b86554b084af7c01157a
Amy DiGiovanni <amydigiov@gmail.com>
2025-08-22 11:57:08 -0700

    * reintroduce tabletop calibration, with toggleable calibration mode * temporarily remove instrument spawner * add probuilder * remove spheres and UI from poke interactors * change PlayVolume material to transparent black

--- Assets/Scripts/Tabletop.cs
+++ Assets/Scripts/Tabletop.cs
@@ -1,17 +1,18 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
-using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 
 public class Tabletop : MonoBehaviour
 {
+    const string k_TabletopYOffsetKeyFormat = "MixedInstruments/Tabletop/{0}/YOffset";
+
     [SerializeField]
     XRBaseInteractable m_CalibrationInteractablePrefab;
 
     [SerializeField]
-    float m_CalibrationDelayTime = 1f;
+    Text m_AdjustButtonText;
 
     [SerializeField]
     Transform m_RightEdge;
@@ -31,12 +32,13 @@ public class Tabletop : MonoBehaviour
     Vector3 m_InitialPosition;
     Quaternion m_InitialRotation;
     ARBoundingBox m_BoundingBox;
+    string m_YOffsetKey;
     float m_XScale;
     float m_ZScale;
 
     XRBaseInteractable m_CalibrationInteractable;
     XRPokeFollowAffordance m_CalibrationPokeFollowAffordance;
-    float m_CalibrationStartTime = -1f;
+    bool m_Calibrating;
 
     InstrumentsSpawner m_InstrumentsSpawner;
 
@@ -57,8 +59,8 @@ public class Tabletop : MonoBehaviour
 
     public void SetupFromBoundingBox(ARBoundingBox boundingBox)
     {
-        Debug.Log("SETUP FROM BOUNDING BOX");
         m_BoundingBox = boundingBox;
+        m_YOffsetKey = string.Format(k_TabletopYOffsetKeyFormat, boundingBox.trackableId);
 
         var tablePose = boundingBox.pose;
         // offset a bit to give room to push down for alignment
@@ -81,13 +83,29 @@ public class Tabletop : MonoBehaviour
         m_FrontEdge.SetZPosition(m_ZScale * -0.5f);
         Debug.Log("SET TABLETOP DIMENSIONS");
 
+        if (PlayerPrefs.HasKey(m_YOffsetKey))
+        {
+            Debug.Log("RESTORE TABLETOP " + boundingBox.trackableId);
+            var yOffset = PlayerPrefs.GetFloat(m_YOffsetKey);
+            transform.position += Vector3.up * yOffset;
+        }
+
         //InitiateCalibration();
-        instrumentsSpawner.Spawn(m_XScale, m_ZScale);
+        //instrumentsSpawner.Spawn(m_XScale, m_ZScale);
+    }
+
+    public void ToggleCalibrationMode()
+    {
+        if (m_Calibrating)
+            ConfirmCalibration();
+        else
+            InitiateCalibration();
     }
 
     void InitiateCalibration()
     {
-        m_CalibrationStartTime = -1f;
+        m_Calibrating = true;
+        m_AdjustButtonText.text = "Confirm";
         m_CalibrationInteractable = Instantiate(m_CalibrationInteractablePrefab, transform);
 
         m_CalibrationPokeFollowAffordance = m_CalibrationInteractable.GetComponentInChildren<XRPokeFollowAffordance>();
@@ -99,35 +117,22 @@ public class Tabletop : MonoBehaviour
         }
 
         m_CalibrationInteractable.transform.localScale = new Vector3(m_XScale, m_CalibrationInteractable.transform.localScale.y, m_ZScale);
-
-        // Confirm calibration after first poke ends, with delay to accomodate adjustments
-        m_CalibrationInteractable.hoverExited.AddListener(OnCalibrationHoverEnded);
-
-        transform.position = m_InitialPosition;
-    }
-
-    void Update()
-    {
-        if (m_CalibrationStartTime > 0f && Time.time - m_CalibrationStartTime >= m_CalibrationDelayTime)
-            ConfirmCalibration();
-    }
-
-    void OnCalibrationHoverEnded(HoverExitEventArgs args)
-    {
-        m_CalibrationStartTime = Time.time;
     }
 
     void ConfirmCalibration()
     {
-        m_CalibrationStartTime = -1f;
-        transform.position = new Vector3(transform.position.x, m_CalibrationPokeFollowAffordance.pokeFollowTransform.position.y, transform.position.z);
+        var yOffset = m_CalibrationPokeFollowAffordance.pokeFollowTransform.position.y - m_InitialPosition.y;
+        transform.position = m_InitialPosition + Vector3.up * yOffset;
+        PlayerPrefs.SetFloat(m_YOffsetKey, yOffset);
         CleanupCalibration();
 
-        instrumentsSpawner.Spawn(m_XScale, m_ZScale);
+        //instrumentsSpawner.Spawn(m_XScale, m_ZScale);
     }
 
     void CleanupCalibration()
     {
+        m_Calibrating = false;
+        m_AdjustButtonText.text = "Adjust";
         if (m_CalibrationInteractable != null)
             Destroy(m_CalibrationInteractable.gameObject);
     }
@@ -135,7 +140,7 @@ public class Tabletop : MonoBehaviour
     public void ResetCalibration()
     {
         CleanupCalibration();
-        instrumentsSpawner.Despawn();
-        InitiateCalibration();
+        transform.position = m_InitialPosition;
+        //instrumentsSpawner.Despawn();
     }
 }
